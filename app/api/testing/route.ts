ef63730686ad1c414f6c684c6270927fa2ad7d8d
David Nartey <dnartey.e@gmail.com>
2025-03-13 03:39:43 +0000

    completed middleware

--- app/api/testing/route.ts
+++ app/api/testing/route.ts
@@ -2,18 +2,15 @@ import { Shipment } from "@/models/shipment-model";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const contentType = request.headers.get("content-type");
-  if (!contentType?.includes("application/json")) {
-    return NextResponse.json(
-      { error: "Invalid content type. Use application/json" },
-      { status: 415 },
-    );
-  }
-
   const body = await request.json();
   try {
     const shipments = await Shipment.findAll();
-    NextResponse.json(shipments, { status: 200 });
-  } catch (error) {}
-  return NextResponse.json(body, { status: 201 });
+    return NextResponse.json(shipments, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching shipments:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
 }
