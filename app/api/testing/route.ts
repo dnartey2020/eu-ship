b3bf76296c20ca20875750a4c37c6245f994352c
David Nartey <dnartey.e@gmail.com>
2025-03-13 03:18:49 +0000

    completed middleware

--- app/api/testing/route.ts
+++ app/api/testing/route.ts
@@ -1,6 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+  const contentType = request.headers.get("content-type");
+  if (!contentType?.includes("application/json")) {
+    return NextResponse.json(
+      { error: "Invalid content type. Use application/json" },
+      { status: 415 },
+    );
+  }
   const body = await request.json();
   return NextResponse.json(body, { status: 201 });
 }
