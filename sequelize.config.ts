a8e83f9b510f466874125e40b061461f5d03b697
David Nartey <dnartey.e@gmail.com>
2025-03-13 04:17:20 +0000

    completed middleware

--- sequelize.config.ts
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  config: path.resolve(__dirname, "config/config.ts"),
-  "models-path": path.resolve(__dirname, "models"),
-  "migrations-path": path.resolve(__dirname, "migrations"),
-  "seeders-path": path.resolve(__dirname, "seeders"),
-};
